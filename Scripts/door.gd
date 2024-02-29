2343f36bdd4357776d223c12f2e96db0d4f1a76e
meursw <filippos-str@hotmail.gr>
2024-02-29 16:15:48 +0200

    door can now close

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -3,9 +3,16 @@ extends CSGBox3D
 @export var switch: Switch
 @export var door_frame: CSGBox3D
 
+var open := false
+
 func _ready():
 	switch.activate.connect(func():
 		var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
-		tween.tween_property(door_frame,"size:y",self.size.y,1.0)
+		if not open:
+			open = true
+			tween.tween_property(door_frame,"size:y",self.size.y,1.0)
+		elif open:
+			open = false
+			tween.tween_property(door_frame,"size:y",0.0,1.0)
 	)
 	
